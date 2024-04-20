5812f0a8d8ddf3e0bead897a37041a3f1b0bb8c7
AsyaSargsyan007 <160397201+AsyaSargsyan007@users.noreply.github.com>
2024-04-20 20:12:21 +0000

    Начало

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+def home():
+    return render_template("home.html",)
+
+@app.route('/create', methods=['GET','POST'])
+def create():
+    if request.method == 'POST':
+        file_name = request.form.get('file_name')
+        file_context = request.form.get('file_context')
+        file_date = request.form.get('file_date')
+        fw = open(file_name, 'w')
+        fw.write(file_context)
+        fw.write(file_date)
+        fw.close()
+
+        return render_template('create.html')
+    return redirect ('/')
